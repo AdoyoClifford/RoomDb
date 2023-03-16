ddc2135b4bccfcd4db923506e51eb4a4155951ab
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-16 13:12:51 +0300

    contact screen working

--- app/src/main/java/com/adoyo/ContactScreen.kt
+++ app/src/main/java/com/adoyo/ContactScreen.kt
@@ -2,19 +2,32 @@
 
 package com.adoyo
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ContactScreen(
@@ -32,9 +45,46 @@ fun ContactScreen(
         }
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(16.dp)
         ) {
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState()),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    SortType.values().forEach { sortType ->
+                        Row(
+                            modifier = Modifier.clickable {
+                                onEvent(ContactEvent.SortContact(sortType))
+                            }, verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = state.sortType == sortType,
+                                onClick = { onEvent(ContactEvent.SortContact(sortType)) })
+                            Text(text = sortType.name)
+
+                        }
+                    }
+                }
+            }
+            items(state.contacts) { contacts ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "${contacts.firstName} ${contacts.lastName}", fontSize = 20.sp)
+                        Text(text = contacts.phoneNumber, fontSize = 12.sp)
+                    }
+                    IconButton(onClick = { onEvent(ContactEvent.DeleteContact(contacts)) }) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Contact")
+
+                    }
+
+                }
+            }
 
         }
     }
