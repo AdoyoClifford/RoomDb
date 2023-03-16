6c4c5b772cd12897938cbd799adc3ebdcc059ca1
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-16 11:58:55 +0300

    viewModel complete

--- app/src/main/java/com/adoyo/ContactViewModel.kt
+++ app/src/main/java/com/adoyo/ContactViewModel.kt
@@ -2,15 +2,35 @@ package com.adoyo
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class ContactViewModel(
     private val dao: ContactDao
 ) : ViewModel() {
     private val _sortType = MutableStateFlow(SortType.FIRST_NAME)
+    private val _contacts = _sortType
+        .flatMapLatest { sortType ->
+            when (sortType) {
+                SortType.FIRST_NAME -> dao.getContactsOrderedByFirstName()
+                SortType.LAST_NAME -> dao.getContactsOrderedByLastName()
+                SortType.PHONE_NUMBER -> dao.getContactsOrderedByPhoneNumber()
+            }
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     private val _state = MutableStateFlow(ContactState())
+    val state = combine(_state, _contacts, _sortType) { state, contacts, sortType ->
+        state.copy(
+            contacts = contacts,
+            sortType = sortType
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ContactState())
 
     fun onEvent(event: ContactEvent) {
         when (event) {
@@ -24,7 +44,32 @@ class ContactViewModel(
                 _state.update { it.copy(isAddingContact = false) }
             }
 
-            ContactEvent.SaveContact -> TODO()
+            ContactEvent.SaveContact -> {
+                val firstName = state.value.firstName
+                val lastName = state.value.lastName
+                val phoneNumber = state.value.phoneNumber
+
+                if (firstName.isBlank() || lastName.isBlank() || phoneNumber.isBlank()) {
+                    return
+                }
+                val contact = Contacts(
+                    firstName = firstName,
+                    lastName = lastName,
+                    phoneNumber = phoneNumber
+                )
+                viewModelScope.launch {
+                    dao.upsertContact(contact)
+                }
+                _state.update {
+                    it.copy(
+                        isAddingContact = false,
+                        firstName = "",
+                        lastName = "",
+                        phoneNumber = ""
+                    )
+                }
+            }
+
             is ContactEvent.SetFirstName -> {
                 viewModelScope.launch {
                     _state.update { it.copy(firstName = event.firstName) }
