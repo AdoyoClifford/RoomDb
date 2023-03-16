c37ab929bfcb813166eb205e414f161e662090f6
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-16 12:56:12 +0300

    not yet implemented contact screen

--- app/src/main/java/com/adoyo/ContactScreen.kt
+++ app/src/main/java/com/adoyo/ContactScreen.kt
@@ -1,2 +1,42 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.adoyo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ContactScreen(
+    state: ContactState,
+    onEvent: (ContactEvent) -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onEvent(ContactEvent.ShowDialog) }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }) { _ ->
+        if (state.isAddingContact) {
+            AddContactDialog(state = state, onEvent = onEvent)
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            contentPadding = PaddingValues(16.dp)
+        ) {
+
+        }
+    }
+
+}
